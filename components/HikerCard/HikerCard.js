b39489107c9a1c6ac1d243b1536cce7cf3b319b6
Matt-Wigg <m.p.wigglesworth@gmail.com>
2022-10-17 19:37:41 -0700

    HikerCard files

---
+++ components/HikerCard/HikerCard.js
@@ -0,0 +1,11 @@
+import styles from './HikerCard.module.css';
+
+const HikerCard = () => {
+  return (
+    <div className={styles.container}>
+      <div>hello</div>
+    </div>
+  );
+};
+
+export default HikerCard;
--- components/layout/Layout.js
+++ components/layout/Layout.js
@@ -1,10 +1,12 @@
 // layout components
 import Nav from './Nav/Nav';
+import HikerCard from '../HikerCard/HikerCard';
 
 const Layout = ({ children }) => {
   return (
     <div>
       <Nav />
+      <HikerCard images={'Images Placeholder'} />
       <main>{children}</main>
     </div>
   );
